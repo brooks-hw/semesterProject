bcf8cfeca128ea45a6ebde620bad757a715c3b25
ryanhaugen <ryanhaugen@protonmail.com>
2025-02-16 15:38:04 -0800

    adding main

---
+++ src/models/Main.java
@@ -0,0 +1,12 @@
+package models;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Investor Buddy"); // Create JFrame with a title
+        frame.setSize(900, 600); // Set size to 300x200
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Close on exit
+        frame.setVisible(true);
+    }
+}
