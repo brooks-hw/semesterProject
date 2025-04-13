73a891bd988703e18417f560b41db427f7afffc7
mubarkk <mubark.kalil@gmail.com>
2025-04-13 01:26:11 -0700

    added line chart

--- src/ui/HomePage.java
+++ src/ui/HomePage.java
@@ -2,14 +2,37 @@ package ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.List;
+import java.util.ArrayList;
 
 public class HomePage extends JPanel {
     public HomePage() {
         setLayout(new BorderLayout());
 
+        JPanel contentPanel = new JPanel();
+        contentPanel.setLayout(new BorderLayout());
+
         JLabel welcomeLabel = new JLabel("Welcome to Investment Buddy!", SwingConstants.CENTER);
         welcomeLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        contentPanel.add(welcomeLabel, BorderLayout.NORTH);
 
-        add(welcomeLabel, BorderLayout.CENTER);
+        List<Double> fakeData = getHourlyPrices();
+        LineChartGenerator chartGen = new LineChartGenerator();
+        JPanel chartPanel = chartGen.createLineChart(fakeData);
+
+        JPanel chartContainer = new JPanel(new BorderLayout());
+        chartContainer.setPreferredSize(new Dimension(800, 200));
+        chartContainer.add(chartPanel, BorderLayout.CENTER);
+
+        contentPanel.add(chartContainer, BorderLayout.SOUTH);
+
+        add(contentPanel, BorderLayout.CENTER);
+    }
+    private List<Double> getHourlyPrices() {
+        List<Double> prices = new ArrayList<>();
+        for (int i = 0; i < 24; i++) {
+            prices.add(100 + Math.random() * 10);
+        }
+        return prices;
     }
 }
