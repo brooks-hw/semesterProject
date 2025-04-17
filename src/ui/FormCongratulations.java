9dacb458e1a5e3f067407105be2ff73e27c1ac92
ryanhaugen <ryanhaugen@protonmail.com>
2025-04-16 20:52:31 -0700

    buttons added to Congratulations page (unfunctional)

--- src/ui/FormCongratulations.java
+++ src/ui/FormCongratulations.java
@@ -2,6 +2,8 @@ package ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FormCongratulations extends JPanel {
     public FormCongratulations() {
@@ -24,5 +26,50 @@ public class FormCongratulations extends JPanel {
         add(Box.createVerticalGlue());
         add(labelPanel);
         add(Box.createVerticalGlue());
+
+        //button options
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
+        buttonPanel.setOpaque(false);
+
+        JButton results = new JButton("View Results");
+        results.setFont(new Font("Arial", Font.BOLD, 25));
+        results.setBackground(new Color(184, 134, 11));
+        results.setForeground(Color.WHITE);
+        results.setFocusPainted(false);
+        results.setAlignmentX(Component.CENTER_ALIGNMENT);
+        results.setPreferredSize(new Dimension(220, 45));
+
+        results.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //cardLayout.show(loginPanel, "InvestmentForm");
+            }
+        });
+
+        buttonPanel.add(Box.createVerticalStrut(130));
+        buttonPanel.add(results);
+
+        JButton home = new JButton("Homepage");
+        home.setFont(new Font("Arial", Font.BOLD, 25));
+        home.setBackground(new Color(184, 134, 11));
+        home.setForeground(Color.WHITE);
+        home.setFocusPainted(false);
+        home.setAlignmentX(Component.CENTER_ALIGNMENT);
+        home.setPreferredSize(new Dimension(220, 45));
+
+        home.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //cardLayout.show(loginPanel, "homePage");
+            }
+        });
+
+        buttonPanel.add(Box.createHorizontalStrut(150));
+        buttonPanel.add(home);
+        buttonPanel.add(Box.createVerticalStrut(300));
+
+
+        add(buttonPanel);
     }
 }
