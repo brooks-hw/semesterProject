e266e2e0c84e1eed33d1a18e6e041f4d30200661
unknown <brookshartwell@gmail.com>
2025-04-30 21:49:24 -0700

    cleaned up project directory

--- projAlternateVersion(brooks)/src/ui/StockPanel.java
+++
@@ -1,7 +0,0 @@
-package ui;
-
-public class StockPanel {
-    public static void main(String[] args) {
-
-    }
-}
--- projAlternateVersion(brooks)/src/ui/iScreenManager.java
+++
@@ -1,8 +0,0 @@
-package ui;
-
-//Interface to MainFrame that allows panels to handle switching
-// without having to know about the MainFrame class
-
-public interface iScreenManager {
-    void switchTo(String screenName);
-}
